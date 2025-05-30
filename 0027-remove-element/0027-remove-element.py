5aca690c58568325f4ce15ad620c3b15331ade62
Mher-1902 <sukiasyanmher7@gmail.com>
2025-05-30 22:43:09 +0400

    Time: 0 ms (100%), Space: 12.5 MB (50.76%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        for i in nums[:]:
+            if i == val:
+                nums.remove(i)
+
+                
+                        
+
+
+
+        
