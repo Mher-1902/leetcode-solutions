379431a60cd1f007406bf66dbfe39ea891ac55e6
Mher-1902 <sukiasyanmher7@gmail.com>
2025-06-20 15:40:45 +0400

    Time: 2 ms (86.09%), Space: 17.9 MB (42.19%) - LeetHub

---
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        k = 3
+        total = 0
+        for j in range(0,k+1):
+            sign = (-1) ** j
+            cmb = math.comb(k,j)
+            top = n - j * (limit + 1) + k -1
+            if top >= 0:
+                total += sign * cmb * math.comb(top,k-1)
+        return total
+        
+
+        
